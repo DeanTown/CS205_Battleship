9bebafa3eff2a8dcca52ca09d53d83391ce535e7
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2019-10-21 18:58:31 -0400

    added Board.cpp to CMakeLists executable, fixed .gitignore, created Board class

---
+++ Board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Oliver Reckord Groten on 10/21/19.
+//
+
+#include "Board.h"
---
+++ Board.h
@@ -0,0 +1,19 @@
+//
+// Created by Oliver Reckord Groten on 10/21/19.
+//
+
+#ifndef CS205_FINALPROJECT_BOARD_H
+#define CS205_FINALPROJECT_BOARD_H
+
+#include "Cell.h"
+#include <vector>
+
+class Board {
+private:
+    std::vector<std::vector<Cell>> board;
+public:
+    /* Constructors */
+};
+
+
+#endif //CS205_FINALPROJECT_BOARD_H
