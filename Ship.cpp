cea16b90203a4a52b34d370cf65a57e0ccadc172
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2019-10-22 12:47:53 -0400

    write Ship class

--- Ship.cpp
+++ Ship.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Ship.h"
+using namespace std;
+
+/* Constructor */
+Ship::Ship() {
+    coords.resize(size);
+}
+
+/* Getters */
+int Ship::getSize() {
+    return size;
+}
+bool Ship::getDestroyed() {
+    return destroyed;
+}
+vector<vector<int>> Shop::getCoords() {
+    return coords;
+}
+
+/* Setters */
+void Ship::setSize(int s) {
+    size = s;
+}
+void Ship::setDestroyed(bool d) {
+    destroyed = d;
+}
+void Ship::setCoords(vector<vector<int>> c) {
+    coords = c;
+}
