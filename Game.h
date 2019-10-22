08dc9348405de56926bbf0ca76042ac91fd870dc
harrymakovsky <hmakovsk@uvm.edu>
2019-10-22 12:34:32 -0400

    fixed

---
+++ Game.h
@@ -0,0 +1,14 @@
+//
+// Created by Harry Makovsky on 10/22/19.
+//
+
+#ifndef CS205_FINALPROJECT_GAME_H
+#define CS205_FINALPROJECT_GAME_H
+
+
+class Game {
+
+};
+
+
+#endif //CS205_FINALPROJECT_GAME_H
