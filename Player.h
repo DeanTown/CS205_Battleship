ef26f1ff28609407ab633e5ebb22bc9067c86a40
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2019-10-22 12:34:36 -0400

    create Player class

---
+++ Player.h
@@ -0,0 +1,14 @@
+//
+// Created by Oliver Reckord Groten on 10/22/19.
+//
+
+#ifndef CS205_FINALPROJECT_PLAYER_H
+#define CS205_FINALPROJECT_PLAYER_H
+
+
+class Player {
+
+};
+
+
+#endif //CS205_FINALPROJECT_PLAYER_H
