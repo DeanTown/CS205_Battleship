84009b26a9ca343c0559a347bd15a511ebd02138
harrymakovsky <hmakovsk@uvm.edu>
2019-10-31 17:38:17 -0400

    working on game class

--- Game.cpp
+++ Game.cpp
@@ -1,27 +1,56 @@
-////
-//// Created by Harry Makovsky on 10/22/19.
-////
 //
-//#include "Game.h"
-//
-////Game::Game(Player p1, Player p2){
-////    player1=p1;
-////    player2=p2;
-////    gameBoard= Board();
-////}
-////
-//////To Begin and end games
-////void Game::startGame(){
-////
-////}
-////
-////void Game::resetGame(){
-////
-////}
-////void Game::endGame(){
-////
-////}
-////
-////void Game::playGame(){
-////
-////}
+// Created by Harry Makovsky on 10/22/19.
+
+
+#include "Game.h"
+#include "stdio.h"
+#include "iostream"
+
+using namespace std;
+
+Game::Game(){
+    compBoard= ComputerBoard();
+    userBoard= UserBoard();
+}
+
+//To Begin and end games
+void Game::startGame(){
+
+    string enter;
+    cout<<"Hello Welcome to BattleShip"<<endl;
+    cout<<"Press enter to start loading pieces";
+    cin>> enter;
+    userBoard.placePieces();
+    compBoard.placePieces();
+}
+
+void Game::resetGame(){
+
+}
+void Game::endGame(){
+
+}
+
+void Game::playGame(){
+    int row;
+    int col;
+    cout<<"OK now that the pieces are placed, lets begin the game\n";
+
+    while(!compBoard.getFleet().sunk() and !userBoard.getFleet().sunk()){
+        compBoard.printBoard();
+
+        cout<<"What row?"<<endl;
+        cin>> row;
+        cout<<"What col?"<<endl;
+        cin>> col;
+
+        compBoard.userMove(row,col);
+
+        wait(new int(3));
+
+
+    }
+
+
+
+}
--- Game.h
+++ Game.h
@@ -5,26 +5,26 @@
 #ifndef CS205_FINALPROJECT_GAME_H
 #define CS205_FINALPROJECT_GAME_H
 
-#include "Player.h"
 #include "Board.h"
+#include "UserBoard.h"
+#include "ComputerBoard.h"
 using namespace std;
 
 class Game {
-//private:
-//    Player player1;
-//    Player player2;
-//    Board gameBoard;
-//    bool p1Turn;
-//    bool p2Turn;
-//public:
-//    /* Constructor */
-//    Game(Player p1, Player p2);
-//    //To Begin and end games
-//    void startGame();
-//    void resetGame();
-//    void endGame();
-//    void playGame();
-//};
+private:
+
+    UserBoard userBoard;
+    ComputerBoard compBoard;
+
+public:
+    /* Constructor */
+    Game();
+    //To Begin and end games
+    void startGame();
+    void resetGame();
+    void endGame();
+    void playGame();
+};
 
 
 #endif //CS205_FINALPROJECT_GAME_H
