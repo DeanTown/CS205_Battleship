48bcaedc07da64bd81958915b4206016dfa58872
Oliver Reckord Groten <oliver.reckordgroten@gmail.com>
2019-10-31 16:45:44 -0400

    Begin work on ship and fleet classes

--- Fleet.cpp
+++ Fleet.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "Fleet.h"
+
+Fleet::Fleet() {}
+
+bool Fleet::sunk() {
+    if (carrier.getDestroyed() &&
+        battleship.getDestroyed() &&
+        destroyer.getDestroyed() &&
+        sub.getDestroyed() &&
+        cruiser.getDestroyed()) {
+        return true;
+    }
+    return false;
+}
--- Fleet.h
+++ Fleet.h
@@ -5,9 +5,18 @@
 #ifndef CS205_FINAL_PROJECT_FLEET_H
 #define CS205_FINAL_PROJECT_FLEET_H
 
+#include "Ship.h"
 
 class Fleet {
-
+private:
+    Ship carrier = Ship(5);
+    Ship battleship = Ship(4);
+    Ship destroyer = Ship(3);
+    Ship sub = Ship(3);
+    Ship cruiser = Ship(2);
+public:
+    Fleet();
+    bool sunk();
 };
 
 
