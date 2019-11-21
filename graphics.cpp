2e9888fe002ae4ed5f337c4ecd2ecde129842132
harrymakovsky <hmakovsk@uvm.edu>
2019-11-21 12:40:34 -0500

    merging

--- graphics.cpp
+++ graphics.cpp
@@ -20,8 +20,13 @@ int wd;
 Board board;
 
 
+enum gameState{menu,game,hitSelection,bye,help};
+
+gameState screen;
+
 
 void init() {
+    screen= menu;
     width = 500;
     height = 500;
 }
@@ -45,21 +50,36 @@ void display(){
     glClear(GL_COLOR_BUFFER_BIT);   // Clear the color buffer with current clearing color
 
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-//    board.draw_board();
-//    glColor3f(1.0,0.0,0.0);
-//    glBegin(GL_QUADS);
-//    glVertex2d(0.0,0.0);
-//    glVertex2d(100.0,0.0);
-//    glVertex2d(100.0,100.0);
-//    glVertex2d(0.0,100.0);
-//    glEnd();
-//    glColor3f(0.0,1.0,0.0);
-//    glBegin(GL_QUADS);
-//    glVertex2d(10.0,10.0);
-//    glVertex2d(90.0,10.0);
-//    glVertex2d(90.0,90.0);
-//    glVertex2d(10.0,90.0);
-//    glEnd();
+
+    switch(screen) {
+        case menu:
+            glColor3f(1.0,0.0,0.0);
+            glBegin(GL_QUADS);
+            glVertex2d(0.0,0.0);
+            glVertex2d(100.0,0.0);
+            glVertex2d(100.0,100.0);
+            glVertex2d(0.0,100.0);
+            glEnd();
+            glColor3f(0.0,1.0,0.0);
+            glBegin(GL_QUADS);
+            glVertex2d(10.0,10.0);
+            glVertex2d(90.0,10.0);
+            glVertex2d(90.0,90.0);
+            glVertex2d(10.0,90.0);
+            glEnd();
+            break;
+        case game:
+            break;
+        case hitSelection:
+            break;
+        case bye:
+            break;
+        case help:
+            break;
+
+    }
+
+    glEnd();
     glFlush();  // Render now
 }
 
@@ -109,6 +129,13 @@ void cursor(int x, int y) {
 // state will be GLUT_UP or GLUT_DOWN
 void mouse(int button, int state, int x, int y) {
 
+//    while(state == GLUT_DOWN){
+//        drag(getShip(x,y),x,y);
+//        glutPostRedisplay();
+//    }
+
+
+
 }
 void timer(int dummy) {
 
@@ -121,11 +148,11 @@ void timer(int dummy) {
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
 
-    Game game= Game();
-    if(game.startGame()){
-        game.playGame();
-        game.endGame();
-    }
+//    Game game= Game();
+//    if(game.startGame()){
+//        game.playGame();
+//        game.endGame();
+//    }
 
 //    UserBoard user;
 //    user.placePieces();
@@ -197,46 +224,46 @@ int main(int argc, char** argv) {
 
 
 
-//    init();
-//
-//    // Initialize GLUT
-//    glutInit(&argc, argv);
-//
-//    glutInitDisplayMode(GLUT_RGBA);
-//
-//    glutInitWindowSize((int)width, (int)height);
-//
-//    // Position the window's initial top-left corner
-//    /* create the window and store the handle to it */
-//    glutInitWindowPosition(-200,-100);
-//
-//    wd = glutCreateWindow("BATTLESHIP" /* title */ );
-//
-//    // Register callback handler for window re-paint event
-//    glutDisplayFunc(display);
-//
-//    // Our own OpenGL initialization
-//    initGL();
-//
-//    // register keyboard press event processing function
-//    // works for numbers, letters, spacebar, etc.
-//
-//    glutKeyboardFunc(kbd);
-//
-//    // register special event: function keys, arrows, etc.
-//    glutSpecialFunc(kbdS);
-//
-//    // handles mouse movement
-//    glutPassiveMotionFunc(cursor);
-//
-//    // handles mouse click
-//    glutMouseFunc(mouse);
-//
-//    // handles timer
-//    glutTimerFunc(0, timer, 0);
-//
-//    // Enter the event-processing loop
-//    glutMainLoop();
+    init();
+
+    // Initialize GLUT
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_RGBA);
+
+    glutInitWindowSize((int)width, (int)height);
+
+    // Position the window's initial top-left corner
+    /* create the window and store the handle to it */
+    glutInitWindowPosition(-200,-100);
+
+    wd = glutCreateWindow("BATTLESHIP" /* title */ );
+
+    // Register callback handler for window re-paint event
+    glutDisplayFunc(display);
+
+    // Our own OpenGL initialization
+    initGL();
+
+    // register keyboard press event processing function
+    // works for numbers, letters, spacebar, etc.
+
+    glutKeyboardFunc(kbd);
+
+    // register special event: function keys, arrows, etc.
+    glutSpecialFunc(kbdS);
+
+    // handles mouse movement
+    glutPassiveMotionFunc(cursor);
+
+    // handles mouse click
+    glutMouseFunc(mouse);
+
+    // handles timer
+    glutTimerFunc(0, timer, 0);
+
+    // Enter the event-processing loop
+    glutMainLoop();
 
     return 0;
 }
